16f55059f0420ebf0e0f3d78d4d583976cf05abc
Tamon <t.t.k6198@icloud.com>
2026-07-16 07:56:13 +0900

    キーボード入力を受け取る処理を追加

--- src/jp/co/wp/sample/ensyuu/Jankencard.java
+++ src/jp/co/wp/sample/ensyuu/Jankencard.java
@@ -1,9 +1,22 @@
 package jp.co.wp.sample.ensyuu;
 
+import java.util.Scanner;
+
 public class Jankencard {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*******************");
 		System.out.println("じゃんけんをしましょう！");
+		System.out.println("*******************");
+		System.out.println("3つから選択してください(g:グー、c:チョキ、p:パー)>");
+		
+		String playerInput = scanner.nextLine();
+		
+		System.out.println("あなたの入力:" + playerInput);
+		
+		scanner.close();
 	}
 
 }
