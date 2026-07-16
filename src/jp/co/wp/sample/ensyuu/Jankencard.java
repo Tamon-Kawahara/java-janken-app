7c7e37561249cc71506a19e3fb9dc8fe180e2e4e
Tamon <t.t.k6198@icloud.com>
2026-07-16 22:36:27 +0900

    5勝するまで繰り返す処理を追加

--- src/jp/co/wp/sample/ensyuu/Jankencard.java
+++ src/jp/co/wp/sample/ensyuu/Jankencard.java
@@ -7,53 +7,65 @@ public class Jankencard {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		int playerWinCount = 0;
+
 		System.out.println("*******************");
 		System.out.println("じゃんけんをしましょう！");
+		System.out.println("5回勝つと終了です。");
 		System.out.println("*******************");
-		System.out.println("3つから選択してください(g:グー、c:チョキ、p:パー)>");
 
-		String playerInput = scanner.nextLine();
+		while (playerWinCount < 5) {
+			System.out.println();
+			System.out.println("現在の勝利数:" + playerWinCount + "勝");
+			System.out.println("3つから選択してください（g:グー、c:チョキ、p:パー）> ");
 
-		String playerHand = "";
+			String playerInput = scanner.nextLine();
 
-		if (playerInput.equals("g")) {
-			playerHand = "グー";
-		} else if (playerInput.equals("c")) {
-			playerHand = "チョキ";
-		} else if (playerInput.equals("p")) {
-			playerHand = "パー";
-		} else {
-			playerHand = "不正な入力";
+			String playerHand = "";
+
+			if (playerInput.equals("g")) {
+				playerHand = "グー";
+			} else if (playerInput.equals("c")) {
+				playerHand = "チョキ";
+			} else if (playerInput.equals("p")) {
+				playerHand = "パー";
+			} else {
+				playerHand = "不正な入力";
+			}
+
+			int computerNumber = (int) (Math.random() * 3);
+
+			String computerHand = "";
+
+			if (computerNumber == 0) {
+				computerHand = "グー";
+			} else if (computerNumber == 1) {
+				computerHand = "チョキ";
+			} else if (computerNumber == 2) {
+				computerHand = "パー";
+			}
+
+			String result = "";
+
+			if (playerHand.equals("不正な入力")) {
+				result = "正しい文字を入力してください";
+			} else if (playerHand.equals(computerHand)) {
+				result = "→Draw";
+			} else if (playerHand.equals("グー") && computerHand.equals("チョキ") ||
+					playerHand.equals("チョキ") && computerHand.equals("パー") ||
+					playerHand.equals("パー") && computerHand.equals("グー")) {
+				result = "→You Win!";
+				playerWinCount++;
+			} else {
+				result = "→You Lose...";
+			}
+			System.out.println("あなた:" + playerHand + " コンピューター:" + computerHand);
+			System.out.println(result);
 		}
 
-		int computerNumber = (int) (Math.random() * 3);
-
-		String computerHand = "";
-
-		if (computerNumber == 0) {
-			computerHand = "グー";
-		} else if (computerNumber == 1) {
-			computerHand = "チョキ";
-		} else if (computerNumber == 2) {
-			computerHand = "パー";
-		}
-
-		String result = "";
-
-		if (playerHand.equals("不正な入力")) {
-			result = "正しい文字を入力してください";
-		} else if (playerHand.equals(computerHand)) {
-			result = "→Draw";
-		} else if (playerHand.equals("グー") && computerHand.equals("チョキ") ||
-				playerHand.equals("チョキ") && computerHand.equals("パー") ||
-				playerHand.equals("パー") && computerHand.equals("グー")) {
-			result = "→You Win!";
-		} else {
-			result = "→You Lose...";
-		}
-
-		System.out.println("あなた:" + playerHand + " コンピューター:" + computerHand);
-		System.out.println(result);
+		System.out.println();
+		System.out.println("5勝しました！");
+		System.out.println("じゃんけんを終了します。");
 
 		scanner.close();
 	}
